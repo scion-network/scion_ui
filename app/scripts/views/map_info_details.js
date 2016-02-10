c7aa173e60a2e06e405ecb19054e5132a15bec8d
Michael Meisinger <michael.meisinger@gmail.com>
2016-02-09 22:21:53 -0800

    Basic visualization

--- app/scripts/views/map_info_details.js
+++ app/scripts/views/map_info_details.js
@@ -24,6 +24,58 @@
         showInstrument: function (item) {
             console.log("Instrument", item, $("#map-info-details"));
             $("#map-info-details").html(this.templateInstDetails(item.toJSON()));
+
+            this.showChart(item);
+        },
+        showChart: function (item) {
+            var self = this;
+            $.ajax({
+                type: 'POST',
+                url: APP.svc_url("scion_management", "get_asset_data"),
+                data: APP.svc_args({
+                    'asset_id': item.id
+                }),
+                success: function (response) {
+                    var result = response.result;
+                    self.updateChart(item, result);
+                }
+            });
+        },
+        updateChart: function (item, result) {
+            var seriesData = [];
+
+            _.forEach(result.data, function(obj, name) {
+                seriesData.push({name: name, data: obj});
+            });
+            var chart = $("#map-details-chart").highcharts({
+                chart: {
+                    type: 'line'
+                },
+                title: {
+                    text: null
+                },
+                xAxis: {
+                    type: 'datetime',
+                    title: {text: "Time"},
+                    dateTimeLabelFormats: {
+                        millisecond: '%H:%M:%S.%L<br>%e-%b-%Y',
+                        second: '%H:%M:%S<br>%e-%b-%Y',
+                        minute: '%H:%M:%S<br>%e-%b-%Y',
+                        hour: '%H:%M<br>%e-%b-%Y',
+                        day: '%e-%b-%Y',
+                        week: '%e-%b-%Y',
+                        month: '%b-%Y',
+                        year: '%Y'
+                    }
+
+                },
+                yAxis: {
+                    title: null,
+                },
+                credits: false,
+                series: seriesData
+            });
+            this.chart = chart;
         },
 
     });
